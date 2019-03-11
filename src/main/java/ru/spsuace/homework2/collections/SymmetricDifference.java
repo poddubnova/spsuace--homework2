e86b6b5cdd42dce8d5797e933503198ae2655c39
poddubnova <dashapoddubnovam@gmail.com>
2019-03-11 09:24:50 +0300

    SymmetricDiff_1

--- src/main/java/ru/spsuace/homework2/collections/SymmetricDifference.java
+++ src/main/java/ru/spsuace/homework2/collections/SymmetricDifference.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
  * Реализуйте симметричную разность двух множеств: https://ru.wikipedia.org/wiki/Симметрическая_разность
  * Исходные множества модифицировать нельзя.
  * Пример: {1, 2, 3} и {0, 1, 2} -> {0, 3}.
- *
  */
 public class SymmetricDifference {
 
