5bf42103cf8350a99b2d43c8900439b0f39ad2c3
poddubnova <dashapoddubnovam@gmail.com>
2019-03-18 09:37:07 +0300

    ComplexNumber

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -1,8 +1,47 @@
 package ru.spsuace.homework2.objects;
 
+import java.util.Objects;
 /**
  * Опишите иммутабельное комплексное число, с геттерами и методами toString()
  * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
  */
 public class ComplexNumber {
+    private final double rePart;
+    private final double imPart;
+
+    ComplexNumber(double re, double im) {
+        this.rePart = re;
+        this.imPart = im;
+    }
+
+    public double getRePart() {
+        return rePart;
+    }
+
+    public double getImPart() {
+        return imPart;
+    }
+
+    @Override
+    public String toString() {
+        return rePart + " + " + imPart + 'i';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        ComplexNumber complexNumber = (ComplexNumber) obj;
+        return (rePart == complexNumber.rePart && imPart == complexNumber.imPart);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rePart, imPart);
+    }
 }
